4a9e30f37babcc7f2dbd6e877f1fe9744fda65e9
baronfel <chusk3@gmail.com>
2011-03-13 12:20:49 -0500

    Adding default constructors for players.
    Testing commits/pushes.....
    
    Change-Id: I68c725426130ff370719386efc6a1c4baa645f70
    Signed-off-by: baronfel <chusk3@gmail.com>

--- Shared/src/edu/spaced/simulation/entity/Player.java
+++ Shared/src/edu/spaced/simulation/entity/Player.java
@@ -11,6 +11,20 @@ public class Player extends Entity {
 	private String name;
 	private State state = State.OBSERVING;
 	
+	public Player()
+	{
+		id = 0;
+		name = "New Player";
+		state = State.OBSERVING;
+	}
+	
+	public Player(int newId, String newName, State newState)
+	{
+		id = newId;
+		name = newName;
+		state = newState;
+	}
+	
 	public boolean equals(Player other) {
 		return other.getId() == id;
 	}
